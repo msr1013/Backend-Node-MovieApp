fb9ef5c8aef8f1ef00166e722ef3da7faea33ac1
Manish Rawal <rawalsinghmanish@gmail.com>
2021-06-18 09:47:46 +0545

    initial-commit

---
+++ routes/courses.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+router.get("/", (req, res) => {
+  res.send(courses);
+});
+
+router.get("/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("the course with given id not found");
+  res.send(course);
+});
+
+router.post("/", (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+router.put("/:id", (req, res) => {
+  //look up the course
+  //if not existing return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("course with given id doesnt exists");
+
+  //validate the course
+  //if invalid ,return 400-bad request
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  //update the course
+  //return updated course to client
+  course.name = req.body.name;
+  res.send(course);
+});
+
+router.delete("/:id", (req, res) => {
+  //look for course
+  //not existing ,return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("the course with id was not found");
+  //delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  //return updated course to client
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  return Joi.validate(course, schema);
+}
+
+module.exports = router;
