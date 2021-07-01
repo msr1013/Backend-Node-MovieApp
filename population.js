28a09dcc62d43ac01eb72b5df415eff2811974f8
Manish Rawal <rawalsinghmanish@gmail.com>
2021-07-01 10:12:44 +0545

    referencing-a-document-in-another-document-in-mongodb

---
+++ population.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("connected to mongoDB.."))
+  .catch((err) => console.error("could not connect to mongoDB..", err));
+
+const Author = mongoose.model(
+  "Author",
+  new mongoose.Schema({
+    name: String,
+    bio: String,
+    website: String,
+  })
+);
+
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Author",
+    },
+  })
+);
+
+async function createAuthor(name, bio, website) {
+  const author = new Author({
+    name,
+    bio,
+    website,
+  });
+
+  const result = await author.save();
+  console.log(result);
+}
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() {
+  const courses = await Course.find().select("name");
+  console.log(courses);
+}
+
+// createAuthor("Manish", "My bio", "My website");
+
+createCourse("Node Course", "60dd429fd407c30d005c6ec8");
---
+++ routes/x.js
@@ -0,0 +1,32 @@
+// trade off between query performance vs consistency
+
+// using references (Normalization)
+let author = {
+  name: "Manish Rawal",
+};
+
+let course = {
+  author: "id",
+};
+
+//using embedded documents(denormalization)
+
+let course = {
+  author: {
+    name: "Manish Rawal",
+  },
+};
+
+//hybrid approach
+
+let author = {
+  name: "Manish",
+  //50 other properties
+};
+
+let course = {
+  author: {
+    id: "ref",
+    name: "Manish",
+  },
+};
