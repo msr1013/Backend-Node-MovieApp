07a80c4467d9a8627d5cceb6646f9b91cecfbf91
Manish Rawal <rawalsinghmanish@gmail.com>
2021-07-11 08:58:51 +0545

    'testing-authentication-of-auth-route-with-email-and-password'

--- routes/auth.js
+++ routes/auth.js
@@ -4,19 +4,21 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const { User } = require("../models/user");
+const Joi = require("joi");
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("Invalid Email or Password.");
+  if (!user) return res.status(400).send("Invalid email or password.");
 
-  const validPassword = bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send("Invalid Email or Password.");
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
 
   res.send(true);
 });
+
 function validate(req) {
   const schema = {
     email: Joi.string().min(5).max(255).required().email(),
