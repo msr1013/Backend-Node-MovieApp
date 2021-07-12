ad72c526ea5ad784cebb5ac9f15d4c81c03b5f78
Manish Rawal <rawalsinghmanish@gmail.com>
2021-07-12 11:14:29 +0545

    'generating-auth-token-in-mongoose-models-here-in-user-model'

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,3 @@
-const config = require("config");
-const jwt = require("jsonwebtoken");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
@@ -18,10 +16,12 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  const token = user.generateAuthToken();
 
   res.send(token);
 });
+
+//information expert principle
 
 function validate(req) {
   const schema = {
