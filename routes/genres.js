b66616f63e6c85ce96d33a8ca83938b2e81a53a0
Manish Rawal <rawalsinghmanish@gmail.com>
2021-07-14 17:59:52 +0545

    passing-async-handler-to-asynMiddleware-function-and-calling-handler-there

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const asyncMiddleware = require("../middleware/async");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
@@ -6,23 +7,26 @@ const { Genre, validate } = require("../models/genre");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 
-router.get("/", async (req, res, next) => {
-  try {
+router.get(
+  "/",
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort("name");
     res.send(genres);
-  } catch (ex) {
-    next(ex);
-  }
-});
+  })
+);
 
-router.post("/", auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post(
+  "/",
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({ name: req.body.name });
-  genre = await genre.save();
-  res.send(genre);
-});
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
+    res.send(genre);
+  })
+);
 
 router.put("/:id", async (req, res) => {
   const { error } = validate(req.body);
